4b924f17e936ba0c9f59928c5dd11128494fce98
Volkan Şahin <bm.volkansahin@gmail.com>
2016-08-03 20:26:16 +0300

    csv file reader created

--- src/main/java/tr/org/liderahenk/registration/subscriber/CsvReader.java
+++ src/main/java/tr/org/liderahenk/registration/subscriber/CsvReader.java
@@ -4,13 +4,16 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class CsvReader {
 
-	public Map<String, String[]> read(String filePath) {
+	public Map<String, String[]> read(InputStream inputStream) {
 
 		Map<String, String[]> recordsMap = new HashMap<String, String[]>();
 
@@ -19,13 +22,13 @@ public class CsvReader {
 		String line = "";
 
 		try {
-			br = new BufferedReader(new FileReader(filePath));
+			br = new BufferedReader(new InputStreamReader(inputStream, StandardCharsets.UTF_8));
 			while ((line = br.readLine()) != null) {
 
 				String[] record = line.split(cvsSplitBy);
 				if (record.length > 0) {
-					String[] dcParameters = Arrays.copyOfRange(record, 1, record.length);
-					recordsMap.put(record[0], dcParameters);
+					String[] ouParameters = Arrays.copyOfRange(record, 1, record.length);
+					recordsMap.put(record[0], ouParameters);
 				}
 			}
 
